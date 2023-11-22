5988736f521e911bfa1b6480413a864d7b1f5787
whddbs627 <whddbs627@gmail.com>
2023-11-22 13:45:15 +0900

    fix: delete lab

--- screens/Lab.js
+++ screens/Lab.js
@@ -1,111 +1,48 @@
-import React, { useEffect, useRef, useState } from "react";
-import {
-  View,
-  Text,
-  Button,
-  StyleSheet,
-  Animated,
-  ScrollView,
-  Dimensions,
-  TextInput,
-  PanResponder,
-  Easing,
-  TouchableOpacity,
-  Pressable,
-} from "react-native";
-import styled from "styled-components/native";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, {useEffect, useRef} from 'react';
+import {View, Animated, StyleSheet} from 'react-native';
 
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-const Box = styled.View`
-  background-color: blue;
-  width: 50px;
-  height: 50px;
-  border-radius: 50px;
-`;
-const AnimatedBox = Animated.createAnimatedComponent(Box);
-
-export default function Home({ navigation }) {
-  const POSITION = useRef(
-    new Animated.ValueXY({
-      x: SCREEN_WIDTH/2-25,
-      y: SCREEN_HEIGHT/2+150,
-    })
-  ).current;
-
-  const handlePress = (event) => {
-    const { locationX, locationY } = event.nativeEvent;
-    Animated.timing(POSITION, {
-      toValue: { x: locationX-25, y: locationY-25 },
+const MoveXY = () => {
+  const startValue = useRef(new Animated.ValueXY(0, 0)).current;
+  const endValue = 150;
+  const duration = 5000;
+  useEffect(() => {
+    Animated.timing(startValue, {
+      toValue: endValue,
+      duration: duration,
       useNativeDriver: true,
     }).start();
-  };
+  }, [startValue]);
 
   return (
-    <Container>
-      
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <TouchableOpacity onPress={handlePress} style={{backgroundColor:'transparent'}}activeOpacity={1}>
-          <AnimatedBox
-            style={{
-              // X,Y 좌표를 각각 이동할 수 있게 설정하는 것
-              transform: [
-                { translateX: POSITION.x },
-                { translateY: POSITION.y },
-              ],
-
-              // 이 함수를 이용하면 위의 X,Y 좌표를 받오므로 아래와 같이 한번에 작성가능함
-              transform: [...POSITION.getTranslateTransform()],
-            }}
-          ></AnimatedBox>
-          <View
-            style={{
-              padding: 20,
-              backgroundColor: "tranparent",
-              width: SCREEN_WIDTH,
-              height: SCREEN_HEIGHT,
-            }}
-          ></View>
-        </TouchableOpacity>
-      </View>
-      <View
-        style={{ flex: 1, justifyContent: "flex-end", alignItems: "center" }}
-      >
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "flex-end",
-          alignItems: "center",
-          marginBottom: 50,
-        }}
-      >
-        <TouchableOpacity
-          onPress={async () =>
-            console.log(await AsyncStorage.getItem("UserServerAccessToken"))
-          }
-          style={{ backgroundColor: "black", borderRadius: 50 }}
-        >
-          <Text
-            style={{ color: "white", marginHorizontal: 30, marginVertical: 20 }}
-          >
-            TEST
-          </Text>
-        </TouchableOpacity>
-        </View>
-      </View>
-    </Container>
+    <View style={styles.container}>
+      <Animated.View
+        style={[
+          styles.square,
+          {
+            transform: [
+              {
+                translateX: startValue.x,
+              },
+              {translateY: startValue.y},
+            ],
+          },
+        ]}
+      />
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  square: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'blue',
   },
 });
+
+export default MoveXY;
