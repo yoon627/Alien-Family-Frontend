d466628fae83a5a4aa25b327331feea866725a8a
whddbs627 <whddbs627@gmail.com>
2023-11-22 15:52:21 +0900

    fix: navigation bug

--- screens/MainDrawer.js
+++ screens/MainDrawer.js
@@ -13,6 +13,8 @@ import TMI from "./TMI";
 import Settings from "./Settings";
 import MainScreen from "./MainScreen";
 import Logout from "./Logout";
+import FamilyInfo from "./FamilyInfo";
+import Lab from "./Lab"
 
 const Drawer = createDrawerNavigator();
 
@@ -20,9 +22,11 @@ export default function MainDrawer({ navigation }) {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name="MainScreen" component={MainScreen} options={{headershown:false}}/>
+      <Drawer.Screen name="FamilyInfo" component={FamilyInfo}/>
       <Drawer.Screen name="TMI" component={TMI}/>
       <Drawer.Screen name="Settings" component={Settings}/>
       <Drawer.Screen name="Logout" component={Logout}/>
+      <Drawer.Screen name="Lab" component={Lab}/>
     </Drawer.Navigator>
   );
 }
