7f3fe2f878551ea15a996dc0a33601681412879a
whddbs627 <whddbs627@gmail.com>
2023-11-30 21:24:54 +0900

    test: font

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-import React from "react";
-import {StyleSheet} from "react-native";
-import {NavigationContainer} from "@react-navigation/native";
-import {createStackNavigator} from "@react-navigation/stack";
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./views/Login";
 import FirstRegister from "./views/FirstRegister";
 import ChooseCharacter from "./views/ChooseCharacter";
@@ -15,57 +15,68 @@ import LadderScreen from "./views/LadderScreen";
 import RouletteScreen from "./views/RouletteScreen";
 import NewGame from "./views/NewGame";
 import MainScreen from "./views/MainScreen";
-import {Provider} from "react-redux";
+import { Provider as StoreProvider } from "react-redux";
 import ChatRoom from "./views/Chatting";
 import ImageDetailForm from "./views/ImageDetailForm";
 import store from "./redux/store";
 import Attendance from "./views/Attendance";
 import AlbumScreen from "./views/AlbumScreen";
+import { MD3LightTheme as DefaultTheme, PaperProvider } from "react-native-paper";
 
 const Stack = createStackNavigator();
 
+const theme = {
+  ...DefaultTheme,
+  colors:{
+    ...DefaultTheme.colors,
+    primary:'tomamto',
+    secondary:'yellow',
+  },
+};
+
 export default function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Login"
-          screenOptions={{headerShown: false}}
-        >
-          <Stack.Screen name="Login" component={Login}/>
-          <Stack.Screen name="KaKaoLogin" component={KaKaoLogin}/>
-          <Stack.Screen name="First Register" component={FirstRegister}/>
-          <Stack.Screen name="Choose Character" component={ChooseCharacter}/>
-          <Stack.Screen name="Invitation" component={InvitationScreen}/>
-          <Stack.Screen name="ClickBox" component={ClickBox}/>
-          <Stack.Screen name="Ladder" component={LadderScreen}/>
-          <Stack.Screen name="Roulette" component={RouletteScreen}/>
-          <Stack.Screen name="Mole" component={NewGame}/>
-          <Stack.Screen name="Chat" component={ChatRoom}/>
-          <Stack.Screen name="ImageDetailForm" component={ImageDetailForm}/>
-          <Stack.Screen name="AlbumScreen" component={AlbumScreen} />
-          <Stack.Screen
-            name="MainDrawer"
-            component={MainDrawer}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen name="First Start" component={FirstStart}/>
-          <Stack.Screen
-            name="Mini Games"
-            component={MiniGames}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="Home"
-            component={MainDrawer}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen name="MainScreen" component={MainScreen} />
-          <Stack.Screen name="Attendance" component={Attendance} />
-
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <StoreProvider store={store}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="Login"
+            screenOptions={{ headerShown: false }}
+          >
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="KaKaoLogin" component={KaKaoLogin} />
+            <Stack.Screen name="First Register" component={FirstRegister} />
+            <Stack.Screen name="Choose Character" component={ChooseCharacter} />
+            <Stack.Screen name="Invitation" component={InvitationScreen} />
+            <Stack.Screen name="ClickBox" component={ClickBox} />
+            <Stack.Screen name="Ladder" component={LadderScreen} />
+            <Stack.Screen name="Roulette" component={RouletteScreen} />
+            <Stack.Screen name="Mole" component={NewGame} />
+            <Stack.Screen name="Chat" component={ChatRoom} />
+            <Stack.Screen name="ImageDetailForm" component={ImageDetailForm} />
+            <Stack.Screen name="AlbumScreen" component={AlbumScreen} />
+            <Stack.Screen
+              name="MainDrawer"
+              component={MainDrawer}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="First Start" component={FirstStart} />
+            <Stack.Screen
+              name="Mini Games"
+              component={MiniGames}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Home"
+              component={MainDrawer}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="MainScreen" component={MainScreen} />
+            <Stack.Screen name="Attendance" component={Attendance} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </StoreProvider>
   );
 }
 
--- views/Login.js
+++ views/Login.js
@@ -1,7 +1,16 @@
-import React from "react";
-import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useCallback } from "react";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ImageBackground,
+} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { AppLoading } from 'expo';
+import { useFonts } from 'expo-font';
 
 const saveServer = async () => {
   try {
@@ -10,7 +19,6 @@ const saveServer = async () => {
       "FcmServerKey",
       "AAAAUCMBJiU:APA91bEs9fOJNe6l2ILHFI88jep5rw9wqR-qTWWbBrKxj7JQnKQ8ZAp4tJbn_yXcL2aP0ydygPIcT89XB6h38vhIozsJ5J61s7w2znBL9hPQG6a18sQcUFkMitr2pkvoCmmfslVQmk-u"
     );
-    // console.log("set server");
   } catch (error) {
     console.log(error);
   }
@@ -27,111 +35,118 @@ const getData = async () => {
 getData();
 
 const Login = ({ navigation }) => {
+  const [isLoaded] = useFonts({
+    'DNFBitBitv2': require('../assets/fonts/DNFBitBitv2.ttf'),
+  });
+
+  if (!isLoaded) {
+    return <AppLoading />;
+  }
   saveServer();
   return (
-    <View style={styles.container}>
-      <MaterialCommunityIcons name="ufo-outline" size={35} color="black" />
-      <Text style={{ fontSize: 50, fontWeight: 500, marginBottom: 75 }}>
-        UFO
-      </Text>
-      <View>
-        <TouchableOpacity
-          onPress={async () => {
-            const test = await AsyncStorage.getItem("UserServerAccessToken");
-            if (test) {
-              navigation.navigate("MainDrawer");
-            } else {
-              Alert.alert("회원가입해주세요");
-            }
-          }}
-          style={{
-            backgroundColor: "black",
-            borderRadius: 50,
-            alignItems: "center",
-            justifyContent: "center",
-            marginVertical: 20,
-          }}
+    <ImageBackground
+      source={require("../assets/img/loginScreen.png")}
+      style={styles.backgroundImage}
+    >
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 7 }} />
+        <View
+          style={{ flex: 1.5, justifyContent: "center", alignItems: "center" }}
         >
           <Text
             style={{
               color: "white",
-              marginHorizontal: 20,
-              marginVertical: 15,
-              alignItems: "center",
-              justifyContent: "center",
+              fontWeight: "bold",
+              fontSize: 40,
+              lineHeight: 40,
+              marginTop: 20,
+              fontFamily:'DNFBitBitv2',
             }}
           >
-            로그인
+            ALIEN
           </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => navigation.navigate("KaKaoLogin")}
-          style={{
-            backgroundColor: "black",
-            borderRadius: 50,
-            alignItems: "center",
-            justifyContent: "center",
-            marginVertical: 20,
-          }}
-        >
           <Text
             style={{
               color: "white",
-              marginHorizontal: 30,
-              marginVertical: 15,
-              alignItems: "center",
-              justifyContent: "center",
+              fontWeight: "bold",
+              fontSize: 40,
+              lineHeight: 40,
+              fontFamily:'DNFBitBitv2',
             }}
           >
-            카카오계정으로 회원가입
+            FAMILY
           </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => navigation.navigate("First Register")}
+          <Text style={{ color: "white", marginVertical: 10 }}>
+            당신의 가족을 찾아보세요!
+          </Text>
+        </View>
+        <View
           style={{
-            backgroundColor: "black",
-            borderRadius: 50,
-            alignItems: "center",
+            flex: 1.5,
             justifyContent: "center",
-            marginVertical: 20,
+            alignItems: "center",
+            marginVertical: 3,
           }}
         >
-          <Text
+          <View style={{ overflow: "hidden", borderRadius: 15, width: 175 }}>
+            <ImageBackground source={require("../assets/img/myBtn.png")}>
+              <TouchableOpacity
+                onPress={async () => {
+                  const test = await AsyncStorage.getItem(
+                    "UserServerAccessToken"
+                  );
+                  if (test) {
+                    navigation.navigate("MainDrawer");
+                  } else {
+                    Alert.alert("회원가입해주세요");
+                  }
+                }}
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  color: "black",
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 20,
+                    color: "white",
+                    marginHorizontal: 20,
+                    marginVertical: 15,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    fontWeight: "bold",
+                  }}
+                >
+                  Start!
+                </Text>
+              </TouchableOpacity>
+            </ImageBackground>
+          </View>
+          <TouchableOpacity
+            onPress={() => navigation.navigate("KaKaoLogin")}
             style={{
-              color: "white",
-              marginHorizontal: 30,
-              marginVertical: 15,
+              borderRadius: 50,
               alignItems: "center",
               justifyContent: "center",
             }}
           >
-            다음페이지로
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => navigation.navigate("MainDrawer")}
-          style={{
-            backgroundColor: "black",
-            borderRadius: 50,
-            alignItems: "center",
-            justifyContent: "center",
-            marginVertical: 20,
-          }}
-        >
-          <Text
-            style={{
-              color: "white",
-              marginHorizontal: 30,
-              marginVertical: 15,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            메인페이지로
-          </Text>
-        </TouchableOpacity>
+            <Text
+              style={{
+                color: "white",
+                marginHorizontal: 30,
+                marginVertical: 15,
+                alignItems: "center",
+                justifyContent: "center",
+                textDecorationLine: "underline",
+              }}
+            >
+              카카오계정으로 회원가입
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
 
