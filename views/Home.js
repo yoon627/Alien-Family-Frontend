210b2a2e87888351cfd5684fefb969040e48cdfb
zzicmuck <91383940+zzic-muck@users.noreply.github.com>
2023-11-30 14:24:57 +0900

    feat: Add new designs for Home and Chat screens

--- views/Home.js
+++ views/Home.js
@@ -3,6 +3,7 @@ import {
   Alert,
   Animated,
   Image,
+  ImageBackground,
   Modal,
   Pressable,
   StyleSheet,
@@ -230,212 +231,214 @@ export default function Home({ navigation }) {
   };
 
   return (
-    <Container>
-      <View
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-          borderColor: "black",
-          borderWidth: 2,
-          borderRadius: 10,
-        }}
-      >
-        <Text>
-          {"<"}TMI{">"}
-        </Text>
-        <MarqueeText
-          style={{ fontSize: 24 }}
-          speed={0.5}
-          marqueeOnStart={true}
-          loop={true}
-          delay={1000}
+    <ImageBackground
+      source={require("../assets/img/mainScreen.png")}
+      style={styles.backgroundImage}
+    >
+      <Container>
+        <TouchableOpacity
+          onPress={() => setModalVisible(true)}
+          style={{
+            marginTop: "15%",
+          }}
         >
-          {todayTMI}
-        </MarqueeText>
-      </View>
-      <TouchableOpacity
-        onPress={() => setModalVisible(true)}
-        style={{ borderRadius: 5, backgroundColor: "black" }}
-      >
-        <Text style={{ color: "white" }}>TMI 작성</Text>
-      </TouchableOpacity>
-      <View
-        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-      ></View>
-      <View style={styles.centeredView}>{movingObject()}</View>
-      <TouchableOpacity onPress={increasePlantLevel}>
-        {renderFlower()}
-      </TouchableOpacity>
-      {/* <Text>{plant}</Text> */}
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "flex-end",
-          alignItems: "center",
-          marginBottom: 50,
-        }}
-      >
-        <View style={styles.centeredView}>
-          <Modal
-            animationType="slide"
-            transparent={true}
-            visible={modalVisible}
-            onRequestClose={() => {
-              setModalVisible(!modalVisible);
-            }}
-          >
-            <View style={styles.centeredView}>
-              <View style={styles.modalView}>
-                <TextInput
-                  value={TMI}
-                  placeholder="당신의 TMI를 알려주세요!"
-                  onChangeText={onChangeTMI}
-                  multiline={true}
-                  numberOfLines={3}
-                  maxLength={40}
-                  editable={true}
-                  style={{
-                    ...styles.input,
-                    margin: 5,
-                    borderColor: "black",
-                    height: 100,
-                    width: 300,
-                    textAlign: "center",
-                  }}
-                />
-                <View style={{ flexDirection: "row", marginVertical: 10 }}>
-                  <Pressable
-                    style={[styles.button, styles.buttonClose]}
-                    onPress={async () => {
-                      const SERVER_ADDRESS =
-                        await AsyncStorage.getItem("ServerAddress");
-                      const UserServerAccessToken = await AsyncStorage.getItem(
-                        "UserServerAccessToken",
-                      );
-                      await axios({
-                        method: "POST",
-                        url: SERVER_ADDRESS + "/tmi",
-                        headers: {
-                          Authorization: "Bearer: " + UserServerAccessToken,
-                        },
-                        data: {
-                          content: TMI,
-                        },
-                      })
-                        .then(async (resp) => {
-                          //todo
-                          // const writer = await AsyncStorage.getItem("nickname");
-                          // setTodayTMI(writer + ": " + TMI + "  " + todayTMI);
-                          fetchData();
-                        })
-                        .catch(function (error) {
-                          console.log("server error", error);
-                        });
-                      setModalVisible(!modalVisible);
-                    }}
-                  >
-                    <Text style={styles.textStyle}>작성</Text>
-                  </Pressable>
-                  <Pressable
-                    style={[styles.button, styles.buttonClose]}
-                    onPress={() => setModalVisible(!modalVisible)}
-                  >
-                    <Text style={styles.textStyle}>취소</Text>
-                  </Pressable>
-                </View>
-              </View>
-            </View>
-          </Modal>
-        </View>
-        <View style={{ flexDirection: "row" }}>
-          {/* <View style={{ marginHorizontal: 10, marginVertical: 20 }}>
-            <TouchableOpacity
-              onPress={() => setModalVisible(true)}
-              style={{ backgroundColor: "black", borderRadius: 50 }}
+          <Text style={{ color: "white", fontSize: 24 }}>오늘의 TMI</Text>
+        </TouchableOpacity>
+
+        <View style={styles.tmiContainer}>
+          <View style={styles.marqueeWrapper}>
+            <MarqueeText
+              style={styles.marqueeText}
+              speed={0.5}
+              marqueeOnStart
+              loop
+              delay={1000}
             >
-              <Text
-                style={{
-                  color: "white",
-                  marginHorizontal: 30,
-                  marginVertical: 20,
-                }}
-              >
-                TMI 작성
-              </Text>
-            </TouchableOpacity>
-          </View> */}
-          <View style={{ marginHorizontal: 10, marginVertical: 20 }}>
-            <TouchableOpacity
-              onPress={async () => {
-                const SERVER_ADDRESS =
-                  await AsyncStorage.getItem("ServerAddress");
-                const UserServerAccessToken = await AsyncStorage.getItem(
-                  "UserServerAccessToken",
-                );
-                await axios({
-                  method: "GET",
-                  url: SERVER_ADDRESS + "/tmi/check",
-                  headers: {
-                    Authorization: "Bearer: " + UserServerAccessToken,
-                  },
-                })
-                  .then(async (resp) => {
-                    if (resp.data.message != "오늘의 tmi를 작성했습니다.") {
-                      Alert.alert("출석을 위해 TMI를 작성해주세요!");
-                    } else {
-                      await axios({
-                        method: "GET",
-                        url: SERVER_ADDRESS + "/attendance",
-                        headers: {
-                          Authorization: "Bearer: " + UserServerAccessToken,
-                        },
-                      })
-                        .then((resp) => {
-                          Alert.alert(resp.data.message);
-                          if (flower) {
-                            setFlower(false);
-                          } else {
-                            setFlower(true);
-                          }
-                        })
-                        .catch((e) => console.log(e));
-                    }
-                  })
-                  .catch((e) => console.log(e));
+              {todayTMI}
+            </MarqueeText>
+          </View>
+        </View>
+
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        ></View>
+        <View style={styles.centeredView}>{movingObject()}</View>
+        <TouchableOpacity onPress={increasePlantLevel}>
+          {renderFlower()}
+        </TouchableOpacity>
+
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "flex-end",
+            alignItems: "center",
+            marginBottom: 50,
+          }}
+        >
+          <View style={styles.centeredView}>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                setModalVisible(!modalVisible);
               }}
             >
-              <View>
-                <MaterialCommunityIcons
-                  name="watering-can"
-                  size={75}
-                  color="black"
-                />
+              <View style={styles.centeredView}>
+                <View style={styles.modalView}>
+                  <TextInput
+                    value={TMI}
+                    placeholder="당신의 TMI를 알려주세요!"
+                    onChangeText={onChangeTMI}
+                    multiline={true}
+                    numberOfLines={3}
+                    maxLength={40}
+                    editable={true}
+                    style={{
+                      ...styles.input,
+                      margin: 5,
+                      borderColor: "black",
+                      height: 100,
+                      width: 300,
+                      textAlign: "center",
+                    }}
+                  />
+                  <View style={{ flexDirection: "row", marginVertical: 10 }}>
+                    <Pressable
+                      style={[styles.button, styles.buttonClose]}
+                      onPress={async () => {
+                        const SERVER_ADDRESS =
+                          await AsyncStorage.getItem("ServerAddress");
+                        const UserServerAccessToken =
+                          await AsyncStorage.getItem("UserServerAccessToken");
+                        await axios({
+                          method: "POST",
+                          url: SERVER_ADDRESS + "/tmi",
+                          headers: {
+                            Authorization: "Bearer: " + UserServerAccessToken,
+                          },
+                          data: {
+                            content: TMI,
+                          },
+                        })
+                          .then(async (resp) => {
+                            //todo
+                            // const writer = await AsyncStorage.getItem("nickname");
+                            // setTodayTMI(writer + ": " + TMI + "  " + todayTMI);
+                            fetchData();
+                          })
+                          .catch(function (error) {
+                            console.log("server error", error);
+                          });
+                        setModalVisible(!modalVisible);
+                      }}
+                    >
+                      <Text style={styles.textStyle}>작성</Text>
+                    </Pressable>
+                    <Pressable
+                      style={[styles.button, styles.buttonClose]}
+                      onPress={() => setModalVisible(!modalVisible)}
+                    >
+                      <Text style={styles.textStyle}>취소</Text>
+                    </Pressable>
+                  </View>
+                </View>
               </View>
-            </TouchableOpacity>
+            </Modal>
           </View>
-          <View style={{ marginHorizontal: 10, marginVertical: 20 }}>
-            <TouchableOpacity
-              onPress={() => navigation.navigate("Attendance")}
-              style={{ backgroundColor: "black", borderRadius: 50 }}
-            >
-              <Text
-                style={{
-                  color: "white",
-                  marginHorizontal: 30,
-                  marginVertical: 20,
+          <View style={{ flexDirection: "row" }}>
+            <View style={{ marginHorizontal: 10, marginVertical: 20 }}>
+              <TouchableOpacity
+                onPress={async () => {
+                  const SERVER_ADDRESS =
+                    await AsyncStorage.getItem("ServerAddress");
+                  const UserServerAccessToken = await AsyncStorage.getItem(
+                    "UserServerAccessToken",
+                  );
+                  await axios({
+                    method: "GET",
+                    url: SERVER_ADDRESS + "/tmi/check",
+                    headers: {
+                      Authorization: "Bearer: " + UserServerAccessToken,
+                    },
+                  })
+                    .then(async (resp) => {
+                      if (resp.data.message != "오늘의 tmi를 작성했습니다.") {
+                        Alert.alert("출석을 위해 TMI를 작성해주세요!");
+                      } else {
+                        await axios({
+                          method: "GET",
+                          url: SERVER_ADDRESS + "/attendance",
+                          headers: {
+                            Authorization: "Bearer: " + UserServerAccessToken,
+                          },
+                        })
+                          .then((resp) => {
+                            Alert.alert(resp.data.message);
+                            if (flower) {
+                              setFlower(false);
+                            } else {
+                              setFlower(true);
+                            }
+                          })
+                          .catch((e) => console.log(e));
+                      }
+                    })
+                    .catch((e) => console.log(e));
                 }}
               >
-                타임라인
-              </Text>
-            </TouchableOpacity>
+                <View>
+                  <MaterialCommunityIcons
+                    name="watering-can"
+                    size={75}
+                    color="black"
+                  />
+                </View>
+              </TouchableOpacity>
+            </View>
+            <View style={{ marginHorizontal: 10, marginVertical: 20 }}>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("Attendance")}
+                style={{ backgroundColor: "black", borderRadius: 50 }}
+              >
+                <Text
+                  style={{
+                    color: "white",
+                    marginHorizontal: 30,
+                    marginVertical: 20,
+                  }}
+                >
+                  타임라인
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
-      </View>
-    </Container>
+      </Container>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    resizeMode: "cover", // 또는 'contain' 등 이미지 사이즈 조정
+  },
+
+  tmiContainer: {
+    alignItems: "center",
+    marginTop: "10%", // 화면 높이의 30% 지점에 배치
+  },
+  marqueeWrapper: {
+    width: 250, // 원하는 너비 설정
+    height: 50, // 원하는 높이 설정
+    overflow: "hidden", // 영역을 벗어난 부분 숨기기
+  },
+  marqueeText: {
+    fontSize: 24,
+    // 기타 스타일 설정
+  },
+
   container: {
     flex: 1,
     justifyContent: "center",
